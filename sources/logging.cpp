d98fce70a7527a9f9d93ce5d21c11299ea57495e
melnikova.m <92054042+melnikova-maria@users.noreply.github.com>
2022-04-04 12:32:26 +0300

    Update logging.cpp

--- sources/logging.cpp
+++ sources/logging.cpp
@@ -4,12 +4,12 @@
 
 void set_logs(size_t val){
   auto trace_log = logs::add_file_log(
-      keywords::file_name = ****************,
+      keywords::file_name = "../logs_files/log_file_%N",
       keywords::rotation_size = val*1024*1024,
       keywords::format = "[%TimeStamp%][%Severity%][%ThreadID%]: %Message%");
   trace_log->set_filter(logs::trivial::severity >= logs::trivial::trace);
   auto info_log = boost::log::add_file_log(
-      keywords::file_name = ****************,
+      keywords::file_name = "../logs_files/info_file_%N",
       keywords::rotation_size = val*1024*1024,
       keywords::format = "[%TimeStamp%][%Severity%][%ThreadID%]: %Message%");
   info_log->set_filter(logs::trivial::severity >= logs::trivial::info);
